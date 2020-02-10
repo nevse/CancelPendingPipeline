bd5b258ccc75119713a2510b7355a030a38aac90
novikov <eugene.novikov@devexpress.com>
2020-02-10 14:26:23 +0300

    init

---
+++ CancelPendingPipeline/Options.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+using CommandLine;
+
+namespace CancelPendingPipeline {
+    class Options {
+        [Option('p', "project", Required = true, HelpText = "project name, i.e. XpfSchedulerPerformanceTestRunner for example")]
+        public String ProjectName { get; set; }
+
+        [Option('t', "token", Required = true, HelpText = "secret token to access")]
+        public String Token { get; set; }
+    }
+}
---
+++ CancelPendingPipeline/Program.cs
@@ -0,0 +1,39 @@
+﻿using CommandLine;
+using GitLabApiClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CancelPendingPipeline {
+    class Program {
+        static void Main(string[] args) {
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed(RunOptions)
+                .WithNotParsed(HandleParseError);
+        }
+        static void RunOptions(Options options) {
+            DoCancelWork(options).Wait();
+        }
+        static void HandleParseError(IEnumerable<Error> obj) {
+            Console.WriteLine("Wrong params!");
+        }
+
+        async static Task DoCancelWork(Options options) {
+            Console.WriteLine("Start cancelling...");
+            var client = new GitLabClient("http://gitserver", options.Token);
+            var projects = await client.Projects.GetAsync().ConfigureAwait(false);
+            var project = projects.Where(x => string.Equals(x.Name, options.ProjectName, StringComparison.Ordinal)).First();
+            var pipelines = await client.Pipelines.GetAsync(project.Id).ConfigureAwait(false);
+            Console.WriteLine($"Found for {options.ProjectName} {pipelines.Count} pipelines");
+            Console.WriteLine("Start removing pending pipelines...");
+            var pendingPipelines = pipelines.Where(x => x.Status == GitLabApiClient.Models.Pipelines.PipelineStatus.Pending);
+            Console.WriteLine($"Found {pendingPipelines.Count()} pending pipelines. Cancelling them...");
+            foreach (var pipelineToCancel in pendingPipelines) {
+                client.Pipelines.CancelAsync(project.Id, pipelineToCancel.Id).Wait();
+                Console.WriteLine($"  canceled {pipelineToCancel.Id}");
+            }
+            Console.WriteLine("Done!");
+        }
+    }
+}
